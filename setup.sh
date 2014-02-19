0d5a8d7512ab72dc914690941fdb46b1338b7fbf
GrahamH/JamilS <gholker@pivotallabs.com>
2014-02-19 16:07:32 -0500

    SurajC/BrentonM : Renaming threading to threadingLib

--- setup.sh
+++ setup.sh
@@ -9,6 +9,6 @@ mvn -q install:install-file -DgroupId=com.xtreme.network -DartifactId=xl-network
 	-Dversion=1.0 -Dpackaging=jar -Dfile="extras/xtreme-network-v1.0.jar"
 
 echo "Installing Xtreme threading library..."
-mvn -q install:install-file -DgroupId=com.xtreme.threading -DartifactId=xl-threading \
+mvn -q install:install-file -DgroupId=com.xtreme.threadingLib -DartifactId=xl-threading \
 	-Dversion=1.1.1 -Dpackaging=jar -Dfile="extras/xl-threading_lib-android-v1.1.1.jar"
 
