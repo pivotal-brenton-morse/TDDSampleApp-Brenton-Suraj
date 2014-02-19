8765a475e98920de01d7d35445672ae9e8ad1e25
GrahamH/JamilS <gholker@pivotallabs.com>
2014-02-19 16:10:28 -0500

    SurajC/BrentonM : Renaming xl-network to xtreme-network

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ mvn -q install:install-file -DgroupId=com.xtreme.rest -DartifactId=xl-rest \
 	-Dversion=2.0.4 -Dpackaging=jar -Dfile="extras/xl-rest_lib-android-v2.0.4.jar"
 
 echo "Installing Xtreme network library..."
-mvn -q install:install-file -DgroupId=com.xtreme.network -DartifactId=xl-network \
+mvn -q install:install-file -DgroupId=com.xtreme.network -DartifactId=xtreme-network \
 	-Dversion=1.0 -Dpackaging=jar -Dfile="extras/xtreme-network-v1.0.jar"
 
 echo "Installing Xtreme threading library..."
